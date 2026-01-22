b9676fdff5b615895f4310e52c91218546cf6c19
Omgeta <anooptiger@hotmail.com>
2026-01-22 23:07:29 +0800

    Add echo feature

--- src/main/java/Omega.java
+++ src/main/java/Omega.java
@@ -1,18 +1,42 @@
+import java.util.Scanner;
+
 public class Omega {
+    private static final String LINE = "____________________________________________________________";
+    private static final String LOGO = """
+                  ___  _ __ ___   ___  __ _  __ _ 
+                 / _ \\| '_ ` _ \\ / _ \\/ _` |/ _` |
+                | (_) | | | | | |  __/ (_| | (_| |
+                 \\___/|_| |_| |_|\\___|\\__, |\\__,_|
+                                       __/ |      
+                                      |___/       
+                """;
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        String logo = """
-              ___  _ __ ___   ___  __ _  __ _ 
-             / _ \\| '_ ` _ \\ / _ \\/ _` |/ _` |
-            | (_) | | | | | |  __/ (_| | (_| |
-             \\___/|_| |_| |_|\\___|\\__, |\\__,_|
-                                   __/ |      
-                                  |___/       
-            """;
-        System.out.println("Hello from\n" + logo);
-        System.out.println("How may I be of assistance?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Au revoir!");
-        System.out.println("____________________________________________________________");
+        printWrapped(
+                "Hello! I'm",
+                LOGO,
+                "How may I be of assistance?"
+        );
+
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.print("> ");
+            String command = scanner.nextLine().trim();
+            if (command.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            printWrapped(command);
+        }
+
+        printWrapped("Au revoir!");
+    }
+
+    public static void printWrapped(String... messages) {
+        System.out.println(LINE);
+        for (String msg: messages) {
+            System.out.println(msg);
+        }
+        System.out.println(LINE);
     }
 }
