d2bc08199e63ab3e83cf46b9706fc30dddba57e8
Omgeta <anooptiger@hotmail.com>
2026-01-22 22:50:47 +0800

    Rename to Omega

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Omega User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Omega.java
@@ -0,0 +1,13 @@
+public class Omega {
+    public static void main(String[] args) {
+        String logo = """
+              ___  _ __ ___   ___  __ _  __ _ 
+             / _ \\| '_ ` _ \\ / _ \\/ _` |/ _` |
+            | (_) | | | | | |  __/ (_| | (_| |
+             \\___/|_| |_| |_|\\___|\\__, |\\__,_|
+                                   __/ |      
+                                  |___/       
+            """;
+        System.out.println("Hello from\n" + logo);
+    }
+}
